d54a68c9532ab6cac2d87ea397e91bf00e7145e5
Sheshanth-29 <sheshanthsaravanan@gmail.com>
2026-08-20 19:27:43 +0530

    ssl change commit

--- bloodbridge-backend/config/db.js
+++ bloodbridge-backend/config/db.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require("sequelize");
+
 require("dotenv").config();
 
 const sequelize = new Sequelize(
@@ -7,8 +8,16 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
-    logging: false, // set to console.log if you want to see every SQL query run
+    logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
